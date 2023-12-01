9ed9123a2290ac7372efacd53e8083c9723489ec
PavanKumar06 <spavankumard@gmail.com>
2023-11-30 21:39:24 -0500

    Client changes for checkout page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
+import Checkout from './components/Checkout';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Checkout />
       </BrowserRouter>
     </div>
   );
