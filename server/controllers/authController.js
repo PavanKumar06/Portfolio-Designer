996b2761516013a47d7e6e4135b846361dd37bf1
PavanKumar06 <spavankumard@gmail.com>
2023-12-05 03:16:39 -0500

    Backend update role when buying subscription

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (id, email) => {
-    return jwt.sign({_id: id, email: email}, process.env.SECRET_KEY, {expiresIn: '1d'})
+    return jwt.sign({_id: id, email: email}, process.env.SECRET_KEY, {expiresIn: '30m'})
 }
 
 const signup = async (req, res) => {
@@ -10,7 +10,7 @@ const signup = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        const token = createToken(user._id, user.email)
+        const token = createToken(user._id, user.email, user.role)
 
         res.status(200).json({email, token})
     } catch (e) {
@@ -23,7 +23,7 @@ const login = async (req, res) => {
 
     try {
         const user = await User.login(email, password)
-        const token = createToken(user._id, user.email)
+        const token = createToken(user._id, user.email, user.role)
 
         res.status(200).json({email, token})
     } catch (e) {
@@ -31,4 +31,24 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = {signup, login}
+const updateRole = async (req, res) => {
+    const {authorization} = req.headers
+    const role = req.body.role
+
+    if (!authorization) {
+        return res.status(401).json({error: 'Authorization token required'})
+    }
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const {_id} = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.updateRole(_id, role)
+
+        res.status(200).json({user})        
+    } catch (e) {
+        res.status(400).json({error: e.message})
+    }
+}
+
+module.exports = {signup, login, updateRole}
